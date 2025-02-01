66b6a44c8c3bd1e9238db315bf7483d80d67e41b
harshitbansall <harshitbansal587@gmail.com>
2025-02-01 16:54:00 +0530

    feat: add automatic translation method to FAQ model

--- home/models.py
+++ home/models.py
@@ -1,5 +1,12 @@
 from ckeditor.fields import RichTextField
 from django.db import models
+from googletrans import Translator
+
+
+def translate_text(text, dest_lang):
+    translator = Translator()
+    translated = translator.translate(text, dest=dest_lang)
+    return translated.text
 
 
 class FAQ(models.Model):
@@ -8,6 +15,33 @@ class FAQ(models.Model):
 
     def __str__(self):
         return self.question
+
+    def save(self, *args, **kwargs):
+        is_new = self.pk is None
+        super().save(*args, **kwargs)
+        if is_new:
+            translations_qs = FAQTranslation.objects.filter(faq=self)
+            existing_languages = set(
+                translations_qs.values_list('language', flat=True))
+            new_translations = []
+            for lang_code, lang_name in FAQTranslation.LANGUAGE_CHOICES:
+                if lang_code not in existing_languages:
+                    try:
+                        translated_question = translate_text(
+                            self.question, lang_code)
+                        translated_answer = translate_text(
+                            self.answer, lang_code)
+                    except Exception as e:
+                        print(f"Error while translating: {e}")
+                        continue
+                    new_translations.append(FAQTranslation(
+                        faq=self,
+                        language=lang_code,
+                        question=translated_question,
+                        answer=translated_answer
+                    ))
+            if new_translations:
+                FAQTranslation.objects.bulk_create(new_translations)
 
     class Meta:
         db_table = 'faqs'
