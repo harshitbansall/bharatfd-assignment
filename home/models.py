89a146b93401886470738bf5958e459ab254d306
harshitbansall <harshitbansal587@gmail.com>
2025-02-01 16:39:37 +0530

    feat: add FAQ model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
-# Create your models here.
+
+class FAQ(models.Model):
+    question = models.TextField()
+    answer = RichTextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        db_table = 'faqs'
